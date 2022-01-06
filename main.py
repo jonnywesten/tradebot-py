93d24ed1a9e2906fda55c7220975936a0f053ef5
Daniel Elport <dan.elport@gmail.com>
2022-01-07 00:22:31 +0100

    Update .gitignore and main.py

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from dateutil.relativedelta import relativedelta
 
 matplotlib.use('TkAgg')
 
-start = datetime.datetime.now() - relativedelta(years=1)
+start = datetime.datetime.now() - relativedelta(years=5)
 
-amzn = web.DataReader("AMZN", "yahoo", start)
-close = amzn['Close']
-close.plot()
+df = web.DataReader("^GSPC", "yahoo", start)
+print(df)
+close = df['Adj Close']
+volume = df['Volume']
+
+plt.subplot(1, 2, 1)
+plt.plot(close)
+plt.subplot(1, 2, 2)
+plt.plot(volume)
 plt.show()
